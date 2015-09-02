342846fd5be1a4a33c68eefcf745ad195cdd038e
Matyu <martin.snapper@gmail.com>
2015-09-03 00:56:44 +0300

    Piltide positsioon nüüd staatiline - sõltumatu veebilehitseja suurusest, mõned detailid Kadri kohta lisatud

--- index.php
+++ index.php
@@ -1,19 +1,22 @@
 <!DOCTYPE html>
 
-<html lang="en">
+<html lang="et">
 <head>
+
     <meta charset="UTF-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>Kadri's homepage</title>
     <style>
-        img {
-            float: right;
 
-        }
         body {
             background-color: #311804;
         }
         #text {
             color: yellow;
+            position: absolute;
+            display: block;
+            margin-top: 350px;
+            margin-left: 400px;
         }
         li {
             color: lawngreen;
@@ -22,14 +25,53 @@
             color: lightpink;
         }
 
+        #pics img {
+
+            position: fixed;
+            margin-bottom: 20px;
+        }
+
+        #image	{
+            /*float: right;*/
+            margin-left: 440px;
+            width: 380px;
+
+            /*-webkit-box-reflect: below 0*/
+            /*-webkit-gradient(linear, left top, left bottom, from(transparent), color-stop(0.2, transparent), to(white));*/
+        }
+
+        #image2 {
+            /*position: fixed;*/
+            /*display: inline-block;*/
+            width: 400px;
+        }
+
     </style>
 
 </head>
 <body>
+    <div id="pics">
+    <img id="image2" src="kadri2.jpg">
+    <img id="image" src="kadri.jpg">
 
-    <img src="kadri.jpg">
-    <div id="text"></div>
-    <audio autoplay>
+    </div>
+
+
+    <div id="text">
+        <li>
+            <ul>Nimi: Kadri</ul>
+            <ul>Elukoht: Tartu</ul>
+            <ul>Lemmikloom: Must kõrvepiiretega krants Jimmi</ul>
+            <ul>Elu- ja koolikaaslane: Martin</ul>
+            <ul>Kodulinn: Pärnu</ul>
+            <ul>Lemmikvärv: pruun</ul>
+            <ul>Lemmikmaius: Tupla</ul>
+            <ul>Meeldib: "kõvasti ja valesti" laulda...</ul>
+            <ul>Ei meeldi: vihm ja remont</ul>
+        </li>
+    </div>
+
+    <audio >
         <source src="06%20Gin%20Wigmore%20-%20If%20Only.mp3">
     </audio>
 
