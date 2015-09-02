d172239a15b2a0ffcaa1055e5b4812222fa266d9
Matyu <martin.snapper@gmail.com>
2015-09-02 20:44:01 +0300

    Pilt lisatud, asetseb paremas ääres

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+/**
+* Created by PhpStorm.
+* User: Matyu
+* Date: 2.09.2015
+* Time: 18:47
+*/
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Kadri's homepage</title>
+    <style>
+        img {
+            float: right;
+
+        }
+        body {
+            background-color: saddlebrown;
+        }
+        #text {
+            color: yellow;
+        }
+        li {
+            color:
+        }
+    </style>
+
+</head>
+<body>
+
+    <img src="kadri.jpg">
+    <div id="text"></div>
+
+</body>
+</html>
+<?php
